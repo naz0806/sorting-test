e4c7d2595ee80db4e0340e6a2dc8b757e70ab905
naz0806 <62138890+naz0806@users.noreply.github.com>
2020-11-22 22:48:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
-	var row = 1;
-    let counter = 1;
+let counter = 1;
     let data = [];
+    
     function AddData(){
       var button = document.getElementById('button');
       button.addEventListener('click', SaveData, false); //
@@ -27,34 +26,24 @@
     }
     function SaveData(){
 
-	 
-      var UserName = document.getElementById('UserName').value;
-      var UserOccupation = document.getElementById('UserOccupation').value;
+	  let UserName = document.getElementById('UserName');
+      let UserOccupation = document.getElementById('UserOccupation');
       data.push({id: counter, UserName: UserName.value, UserOccupation: UserOccupation.value });
       console.log(data);
       counter++;
       document.getElementById('UserID').value = counter;
-      
-      let outputSection  = document.getElementById('output');
-     
-	  var display = document.getElementById("display");
-      
-      var newRow = display.insertRow(row);
-      
-      var cell1 = newRow.insertCell(0);
-      var cell2 = newRow.insertCell(1);
-      var cell3 = newRow.insertCell(2);
-      
-      cell1.innerHTML = counter - 1;
-      cell2.innerHTML = UserName;
-      cell3.innerHTML = UserOccupation;
+      UserName.value='';
+      UserOccupation.value ='';
 
-	  row++;
-      
+      let outputSection  = document.getElementById('output');
+      let outputData = data[data.length - 1];
+      let newP = document.createElement('p');
+      newP.textContent = outputData['id'] + " " + outputData['UserName'] + "  "+outputData['UserOccupation'];
+      outputSection.appendChild(newP);
       
     }
 
-    function sortByNameOrOccupation(attribute){
+   function sortByNameOrOccupation(attribute){
       data.sort(function(a, b) {
         var nameA = a[attribute].toUpperCase(); // ignore upper and lowercase
         var nameB = b[attribute].toUpperCase(); // ignore upper and lowercase
@@ -65,18 +54,18 @@
           return 1;
         }
 
-        // names must be equal
+        // names is equal
         return 0;
       });
     }
 
- function displaySortedData(){
+ function displaySortedData(display){
       let outputSection  = document.getElementById('output');
       outputSection.innerHTML = '';
       let fragment = document.createDocumentFragment();
       data.forEach(function(d) {
           let p = document.createElement('p');
-          p.textContent = d['id'] + " " + d['name'] + "  "+d['occupation'];
+          p.textContent = d['id'] + " " + d['UserName'] + "  "+d['UserOccupation'];
           fragment.appendChild(p);
       });
       outputSection.appendChild(fragment);
